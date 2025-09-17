b631d81b63b8268c7e6e182ef203a02745c74bdb
Chad Snowden <chadsnowden3@gmail.com>
2025-09-17 15:31:08 -0400

    Update m61.cc

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -1,4 +1,4 @@
-#include "m61.hh" \\ https://github.com/cs61/cs61-f25-psets/blob/main/pset1/m61.hh
+#include "m61.hh" // https://github.com/cs61/cs61-f25-psets/blob/main/pset1/m61.hh
 #include <cstdlib>
 #include <cstddef>
 #include <cstring>
